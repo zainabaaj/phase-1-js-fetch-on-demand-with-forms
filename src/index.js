3dbff2f9d562640229b840d58bcb0057a96c4d34
Zeyna Baaj <zainabaaj@gmail.com>
2023-05-25 15:39:50 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
+
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const input = document.querySelector('input#searchByID');
+
+        console.log(input.value);
+
+        fetch(`./db.json`)
+            .then(response => response.json())
+            .then(data => {
+                const movie = data.movies[input.value - 1]
+                const title = document.querySelector('section#movieDetails h4');
+                const summary = document.querySelector('section#movieDetails p');
+
+                title.innerText = movie.title;
+                summary.innerText = movie.summary;
+            });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
